68781c5725c43a9632c201966381488fc7263dcc
Ernest1915 <ernest.okiror@strathmore.edu>
2025-11-17 18:48:28 +0300

    redid_appwrit

--- src/appwriteconfig/config.ts
+++ src/appwriteconfig/config.ts
@@ -1,8 +1,12 @@
-import {Client, Account} from 'appwrite';
-const client = new Client();
-client 
-  .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('67f39a440025dedaf66c');
+import { Client, Account, Databases, Storage } from "appwrite";
 
-const account = new Account(client);
-export  { client, account};  
+
+const client = new Client()
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT )
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID );
+
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+
